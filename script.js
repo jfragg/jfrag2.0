ac10ea416506eb042aa71016f274b21bd381da7a
Jonathan Fragakis <jfragaki@uwo.ca>
2017-01-03 13:41:26 -0500

    get user input to continue the intro

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
     $(".terminal")
 
     var scenario = 0;
+    var cont = false;
 
     SceneZero();
 
@@ -15,13 +16,14 @@ $(document).ready(function(){
             strings: [intro, intro2],
             typeSpeed: 50,
             callback: function () {
-                setTimeout(500);
-                $(".typed-cursor").remove();
-                $("p").removeClass("active");
-                let newEle = "<p> A.V.A: $ </p> <span class='active'></span>";
-                $(".terminal-content").append(newEle);
-                scenario++;
-                SceneOne();
+                setTimeout(function(){
+                    $(".typed-cursor").remove();
+                    $("p").removeClass("active");
+                    let newEle = "<p> A.V.A: $ </p> <span class='active'></span>";
+                    $(".terminal-content").append(newEle);
+                    scenario++;
+                    SceneOne();
+                }, 500);
             }
         });
     }
@@ -30,33 +32,72 @@ $(document).ready(function(){
         let line1 = "I've done some digging...";
         let line2 = "Here is what I've found on my creator.";
 
-        //let details = FormatJSON(_details);
-
         $(".active").typed({
             strings: [line1, line2],
             typeSpeed: 50,
             callback: function() {
-                setTimeout(500);
-                $("span").removeClass("active")
-                $(".typed-cursor").remove();
-                let newEle = "</br> <p> A.V.A: $ </p> <span class='active'></span>";
-                $(".terminal-content").append(newEle)
-                scenario++;
-                ScenarioTwo();
+                setTimeout(function(){
+                    ReadyScene();
+                    scenario++;
+                    SceneTwo();
+                }, 500);
             }
         });
     }
 
-    function ScenarioTwo(){
+    function SceneTwo(){
         let line1 = "Let me make some room...";
 
         $(".active").typed({
             strings: [line1],
             typeSpeed: 50,
             callback: function() {
-                setTimeout(500);
-                $("span").removeClass("active");
-                MakeRoom();
+                setTimeout(function(){
+                    $("span").removeClass("active");
+                    MakeRoom();
+                    ReadyScene();
+                    SceneTwoFiller();
+                }, 500);
+            }
+        });
+    }
+
+    function SceneTwoFiller(){
+        let line1 = "Shall we continue? (y/n) ";
+        
+        $(".active").typed({
+            strings: [line1],
+            typeSpeed: 50,
+            callback: function(){
+                $(".typed-cursor").remove();
+                let newEle = "<input type='text' id='continue-input'> </input>";
+                $(".active").after(newEle);
+            }
+        });
+    }
+
+    function SceneThree(){
+        let line1 = "Ok, I'll keep looking.";
+
+        $(".active").typed({
+            strings: [line1],
+            typeSpeed: 50, 
+            callback: function() {
+                ReadyScene();
+                SceneThreeFiller();
+            }
+        });
+    }
+
+    function SceneThreeFiller(){
+        let line1 = "Let me just clean this up for you.";
+
+        $(".active").typed({
+            strings: [line1],
+            typeSpeed: 50, 
+            callback: function() {
+                ReadyScene();
+                CleanUpJSON();
             }
         });
     }
@@ -68,5 +109,34 @@ $(document).ready(function(){
             $(".card").delay(250).fadeIn("slow");
         });
     }
+
+    function CleanUpJSON(){
+
+        $(".card").delay(250).fadeOut("slow");
+        $(".terminal").delay(500).animate({
+            left: '20px'
+        }, "slow");
+
+    }
+
+    
+    function ReadyScene(){
+        $("span").removeClass("active")
+        $(".typed-cursor").remove();
+        let newEle = "</br> <p> A.V.A: $ </p> <span class='active'></span>";
+        $(".terminal-content").append(newEle);
+    }
+
+    $(".terminal").on("keypress", "input#continue-input", function(e){
+        if(e.which == 13) {
+            let value = $("#continue-input").val();
+            if(value === 'y'){
+                cont = true;
+                $("#continue-input").attr("disabled", "true");
+                ReadyScene();
+                SceneThree();
+            }
+        }
+    });
 
 });
